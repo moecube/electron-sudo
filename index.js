bb1b1ab5855a68bc90929d83df4be7b3dee6c24f
zh99998 <zh99998@gmail.com>
2016-11-08 12:53:09 +0800

    refactor

---
+++ index.js
@@ -0,0 +1,65 @@
+const os = require('os');
+const net = require('net');
+const path = require('path');
+const readline = require('readline');
+const child_process = require("child_process");
+
+const {remote} = require('electron');
+
+module.exports.fork = function (modulePath, args = [], options = {}) {
+    if (!options.env) {
+        options.env = {}
+    }
+
+    let ipcPath;
+    if (process.platform == 'win32') {
+        ipcPath = path.join('\\\\.\\pipe', os.tmpdir(), Math.random().toString())
+    } else {
+        ipcPath = path.join(os.tmpdir(), Math.random().toString())
+    }
+
+    options.env['ELEVATE'] = JSON.stringify({
+        method: 'fork',
+        arguments: [modulePath, args, options],
+        ipc: ipcPath
+    });
+    let execPath = remote.app.getPath('exe');
+    let spawn_command, spawn_args;
+    switch (process.platform) {
+        case 'darwin':
+            spawn_command = 'osascript';
+            spawn_args = ['-e', `do shell script "${execPath} ." with administrator privileges`];
+            break;
+        case 'win32':
+            spawn_command = path.join(process.env['SystemRoot'], 'WindowsPowerShell', 'v1.0', 'powershell.exe');
+            spawn_args = ['Start-Process', execPath, '.', '-Verb', 'runAs', '-Wait', '-WindowStyle', 'Hidden'];
+            break;
+    }
+    let child = child_process.spawn(spawn_command, spawn_args, options);
+
+    let connection = [];
+    child.send = (message, sendHandle, options, callback)=> {
+        if (Array.isArray(connection)) {
+            connection.push(arguments);
+        } else {
+            connection.write(JSON.stringify(message) + os.EOL, callback);
+        }
+    };
+
+    let server = net.createServer();
+    server.listen(ipcPath);
+    server.once('connection', (conn)=> {
+        for (let message of connection) {
+            child.send.apply(child, message);
+        }
+        connection = conn;
+        readline.createInterface({input: connection}).on('line', (line) => {
+            child.emit('message', JSON.parse(line));
+        });
+
+        // only accept one connection.
+        server.close();
+    });
+    return child
+
+};
