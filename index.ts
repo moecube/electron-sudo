fdc52186d1ddb59a9cfb89caf81faf26fcd97289
zh99998 <zh99998@gmail.com>
2017-06-08 15:49:30 +0800

    ts

--- index.ts
+++ index.ts
@@ -1,28 +1,30 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const child_process = require("child_process");
-const electron_1 = require("electron");
-const net = require("net");
-const os = require("os");
-const path = require("path");
-const readline = require("readline");
-function fork(modulePath, args = [], options = {}) {
+import * as child_process from 'child_process';
+import { ChildProcess, ForkOptions } from 'child_process';
+import { remote } from 'electron';
+import * as net from 'net';
+import { Socket } from 'net';
+import * as os from 'os';
+import * as path from 'path';
+import * as readline from 'readline';
+
+export function fork(modulePath: string, args: string[] = [], options: ForkOptions = {}): ChildProcess {
     if (!options.env) {
         options.env = {};
     }
+
     let ipcPath;
     if (process.platform === 'win32') {
         ipcPath = path.join('\\\\.\\pipe', os.tmpdir(), Math.random().toString());
-    }
-    else {
+    } else {
         ipcPath = path.join(os.tmpdir(), Math.random().toString());
     }
+
     let elevate = new Buffer(JSON.stringify({
         method: 'fork',
         arguments: [modulePath, args, options],
         ipc: ipcPath
     })).toString('base64');
-    let execPath = electron_1.remote.app.getPath('exe');
+    let execPath = remote.app.getPath('exe');
     let spawn_command, spawn_args;
     switch (process.platform) {
         case 'darwin':
@@ -38,34 +40,36 @@ function fork(modulePath, args = [], options = {}) {
             throw 'unsupported platform';
     }
     // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/17017
-    options.stdio = 'ignore';
+    options.stdio = <any>'ignore';
+
     let child = child_process.spawn(spawn_command, spawn_args, options);
-    let connection = [];
+
+    let connection: Socket | IArguments[] = [];
     // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/17018
-    child.send = ((message, sendHandle, _options, callback) => {
+    child.send = <any>((message: any, sendHandle?: any, _options?: any, callback?: any) => {
         if (Array.isArray(connection)) {
             connection.push(arguments);
-        }
-        else {
-            connection.write(JSON.stringify(message) + os.EOL, callback);
+        } else {
+            connection!.write(JSON.stringify(message) + os.EOL, callback);
         }
     });
+
+
     let server = net.createServer();
     server.listen(ipcPath);
     server.once('connection', (conn) => {
-        for (let message of connection) {
+        for (let message of <IArguments[]>connection) {
             child.send.apply(child, message);
         }
         connection = conn;
         // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/17020
-        readline.createInterface({ input: connection }).on('line', (line) => {
+        readline.createInterface({ input: <any>connection }).on('line', (line) => {
             child.emit('message', JSON.parse(line));
         });
+
         // only accept one connection.
         server.close();
     });
     return child;
-}
-exports.fork = fork;
-;
-//# sourceMappingURL=index.js.map
+
+};
